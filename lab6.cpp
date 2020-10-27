1e274d26ce15889822d0bf98f335c1ad463c6d8c
Alie <aboondooley@gmail.com>
2020-10-26 22:17:04 -0700

    Added TODO for tests.

--- lab6.cpp
+++ lab6.cpp
@@ -6,5 +6,15 @@ int main() {
     MaxHeap<int> m;
     cout << m.empty() << endl;
     cout << m.size() << endl;
+    //TODO update so it works with the enum:
+    /*
+     * enum Importance {  // enum classes are like a set of integer constants
+    VERY = 20, SOMEWHAT = 9, BLEH = 0, HARDLY = -3, UNIMPORTANT = -10
+};
+
+// Construction and first enqueue would look like this:
+MaxHeap<Importance> test;
+test.enqueue(HARDLY);
+     */
     return 0;
 }
